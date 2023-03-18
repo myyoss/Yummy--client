3f51adbda5814fcdb91dc01b38f3854ca46cba7b
Yossi <asnafy.junior@gmail.com>
2023-03-18 16:38:42 +0200

    css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./dist/App.css";
+import "./App.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Auth from "./pages/auth";
